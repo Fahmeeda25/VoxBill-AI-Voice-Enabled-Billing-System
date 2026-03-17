9eda727892cec95493bfeaa21098654d1877e111
FAHMEEDA TAHSEEN. S <147984953+Fahmeeda25@users.noreply.github.com>
2026-03-17 23:23:57 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,13 @@
+package com.billing;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Voice Billing System Backend Running!";
+    }
+}
